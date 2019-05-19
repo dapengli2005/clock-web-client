ab3c992f1858eed614c615d94b8963a2608808b1
Dapeng Li <luislee818@gmail.com>
2019-05-19 15:15:22 +0200

    refactor: move logic to header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,71 +1,14 @@
-import { Injectable, Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { User } from './models/user.interface';
-import { UserService } from './user.service';
-
-interface Nav {
-  link: string,
-  name: string,
-  exact: boolean
-}
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      <nav *ngIf="user()" class="nav">
-        <a
-          *ngFor="let item of nav"
-          [routerLink]="item.link"
-          routerLinkActive="active"
-          [routerLinkActiveOptions]="{ exact: item.exact }">
-          {{ item.name }}
-        </a>
-      </nav>
-      <div *ngIf="user()">
-        Hello {{user().username}}.
-        <a href="#" (click)="logout($event)">log out</a>
-      </div>
       <app-header></app-header>
       <router-outlet></router-outlet>
       <app-footer></app-footer>
     </div>
   `
 })
-@Injectable()
 export class AppComponent {
-  nav: Nav[] = [
-    {
-      link: '/clock',
-      name: 'Clock',
-      exact: true
-    },
-    {
-      link: '/clock/history',
-      name: 'History',
-      exact: true
-    },
-    {
-      link: '/clock/new',
-      name: 'New',
-      exact: true
-    }
-  ];
-
-  constructor(private userService: UserService, private router: Router) {
-  }
-
-  user() {
-    return this.userService.getUser();
-  }
-
-  logout(e) {
-    e.preventDefault();
-    this.userService
-      .logout()
-      .subscribe(() => {
-        this.router.navigate(['/']);
-      });
-  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,38 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../models/user.interface';
+import { UserService } from '../user.service';
+
+interface Nav {
+  link: string,
+  name: string,
+  exact: boolean
+}
 
 @Component({
   selector: 'app-header',
   styleUrls: ['./header.component.css'],
   template: `
     <nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top">
-      <a class="navbar-brand" href="#">Angular Bootstrap Demo</a>
+      <a class="navbar-brand" href="#">Clock Me</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse"
         aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
+      <div class="collapse navbar-collapse" id="navbarCollapse" *ngIf="user()">
         <ul class="navbar-nav mr-auto">
-
-          <li class="nav-item">
-            <a class="nav-link" routerLink="/home">Home</a>
+          <li class="nav-item" *ngFor="let item of nav">
+            <a class="nav-link"
+              [routerLink]="item.link"
+              routerLinkActive="active"
+              [routerLinkActiveOptions]="{ exact: item.exact }">
+              {{ item.name }}
+            </a>
           </li>
+        </ul>
+        <ul class="nav navbar-nav ml-auto">
           <li class="nav-item">
-            <a class="nav-link" routerLink="/contact-list">Contacts</a>
+            <a class="nav-link" href="#" (click)="logout($event)">Logoff {{user().username}}</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" routerLink="/contact-create">Create</a>
-          </li>
-
         </ul>
       </div>
     </nav>
   `,
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  nav: Nav[] = [
+    {
+      link: '/clock',
+      name: 'Clock',
+      exact: true
+    },
+    {
+      link: '/clock/history',
+      name: 'History',
+      exact: true
+    },
+    {
+      link: '/clock/new',
+      name: 'New',
+      exact: true
+    }
+  ];
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) {
+  }
 
-  ngOnInit() {
+  user() {
+    return this.userService.getUser();
+  }
+
+  logout(e) {
+    e.preventDefault();
+    this.userService
+      .logout()
+      .subscribe(() => {
+        this.router.navigate(['/']);
+      });
   }
 
 }
