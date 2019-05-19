b11ea1f28853f6fc9768e1e903a5c92e0efd5610
Dapeng Li <luislee818@gmail.com>
2019-05-19 15:46:40 +0200

    feature: add basic styling to 404 page

--- src/app/not-found.component.ts
+++ src/app/not-found.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'not-found',
   template: `
-    <div>
-      Not found, <a routerLink="/">go home</a>?
+    <div class="jumbotron" style="background-color: #fff; height: calc(95vh);">
+      <h3>This page does not exist...</h3>
+      <p class="lead">
+        <a routerLink="/">Go home</a>
+      </p>
     </div>
   `
 })
